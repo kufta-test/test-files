2cc522ced97b3bf5e86269e49490a86799fef1fc
Eric Kufta <eric.kufta@semgrep.com>
2024-03-12 12:51:31 -0700

    fixing javascript vulns

--- javascript/hardcoded-bearer-token.js
+++ javascript/hardcoded-bearer-token.js
@@ -1,19 +1,18 @@
 import axios from 'axios'
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+require('dotenv').config()
 
 axios.post('/api/token', JSON.stringify({
     key: 'aaaaaa'
 })).then(res => {
     // ruleid: hardcoded-bearer-token
-    axios.defaults.headers.common['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    axios.defaults.headers.common['Authorization'] = process.env.TOKEN;
 
     // ruleid: hardcoded-bearer-token
-    axios.defaults.headers.common['Authorization'] = "Bearer "+token;
+    axios.defaults.headers.common['Authorization'] = process.env.TOKEN;
 
 
     // ok: hardcoded-bearer-token
     axios.defaults.headers.common['Authorization'] = "Bearer eexample";
-
 
     // ok: hardcoded-bearer-token
     axios.defaults.headers.common['Authorization'] = "Bearer" + config.value["token"]
