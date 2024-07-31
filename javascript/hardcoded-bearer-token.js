268f868e2cf369a5b445031f8c876801442f38cb
Eric Kufta <eric.kufta@semgrep.com>
2024-07-31 08:15:42 -0700

    commiting

--- javascript/hardcoded-bearer-token.js
+++ javascript/hardcoded-bearer-token.js
@@ -1,21 +1,11 @@
 import axios from 'axios'
 require('dotenv').config()
 
-axios.post('/api/token', JSON.stringify({
+axios.post('/api/token/token', JSON.stringify({
     key: 'aaaaaa'
 })).then(res => {
     // ruleid: hardcoded-bearer-token
     axios.defaults.headers.common['Authorization'] = process.env.TOKEN;
-
-    // ruleid: hardcoded-bearer-token
-    axios.defaults.headers.common['Authorization'] = process.env.TOKEN;
-
-
-    // ok: hardcoded-bearer-token
-    axios.defaults.headers.common['Authorization'] = "Bearer eexample";
-
-    // ok: hardcoded-bearer-token
-    axios.defaults.headers.common['Authorization'] = "Bearer" + config.value["token"]
 
 
 }).catch((error) => {
--- python/command_injection_generic_practice.py
+++ python/command_injection_generic_practice.py
@@ -1,6 +1,5 @@
 import os
 
-
 btype = req.field('backuptype')
 cmd = "cmd.exe /K \"c:\\util\\rmanDB.bat " + btype + "&&c:\\util\\cleanup.bat\""
 # ruleid: command_injection_generic_practice
@@ -9,4 +8,4 @@ os.system(cmd);
 home = os.getenv('APPHOME')
 cmd = home.join(INITCMD)
 # ruleid: command_injection_generic_practice
-os.system(cmd)
+os.system(cmd);
